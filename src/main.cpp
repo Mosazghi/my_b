95dc43507c90178e5644e7310c525089aea21bf5
Mosazghi <fest121@hotmail.com>
2025-11-26 00:15:39 +0100

    Add simple program options

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <cstring>
 #include <iostream>
 #include <memory>
 #include <string>
@@ -6,26 +7,45 @@
 #include "url.h"
 
 static Logger* logger = new Logger("main");
+void print_help(char* argv[]);
+
 int main(int argc, char* argv[]) {
-  std::cout << "Init.";
+  std::string url_addr{};
   logger->inf("Initializing...");
+
   if (argc < 2) {
-    std::cout << "Usage: " << argv[0] << " <url>\n";
-    return 1;
-  }
-  for (int i = 1; i < argc; i++) {
-    std::cout << argv[i] << '\n';
+    std::cout << "Usage: " << argv[0] << " --url <url>\n";
+    exit(EXIT_FAILURE);
   }
 
-  std::string addr = argv[1];
+  for (int i = 1; i < argc; i++) {
+    const std::string& arg{argv[i]};
+    if (arg == "--help" || arg == "-h") {
+      print_help(argv);
+      exit(EXIT_SUCCESS);
+    }
+
+    if (arg == "--url" || arg == "-u") {
+      url_addr = argv[i + 1];
+    }
+  }
+
+  if (url_addr.empty()) {
+    logger->err("URL cannot be empty!");
+    exit(EXIT_FAILURE);
+  }
+
   auto http_client = std::make_shared<http::HttpClient>();
   auto file_client = std::make_shared<file::File>();
-  url::URL url(addr, http_client, file_client);
+  url::URL url(url_addr, http_client, file_client);
   auto response = url.request();
   if (response) {
-    // logger->inf("Body {}", response->body);
     url.show(response->body);
   }
 
   return 0;
+}
+
+void print_help(char* argv[]) {
+  std::cout << "Usage: " << argv[0] << " -u|--url <url>\n";
 }
