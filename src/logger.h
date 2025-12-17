a292b38a7031037c1d0d0d40b59859f73c03cb60
Mosazghi <fest121@hotmail.com>
2025-12-17 02:07:00 +0100

    Improve logger

--- src/logger.h
+++ src/logger.h
@@ -1,99 +1,118 @@
 #pragma once
 #include <chrono>
-#include <cstdarg>
 #include <format>
 #include <iostream>
+#include <mutex>
+#include <source_location>
+#include <string_view>
 
-enum LogLevel { INFO, DBG, WARN, ERROR };
+enum class LogLevel { DBG, INFO, WARN, ERROR };
+
 class Logger {
  public:
-  Logger(std::string_view name) : m_name(name) {}
-  ~Logger() {}
+  explicit Logger(std::string_view name) : m_name(name) {}
+
+  struct LogFormat {
+    std::string_view fmt;
+    std::source_location loc;
+
+    template <typename T>
+    consteval LogFormat(
+        const T& s, std::source_location l = std::source_location::current())
+        : fmt(s), loc(l) {}
+  };
 
   template <typename... Args>
-  void err(std::string_view fmt, Args&&... args) {
-    base(fmt, LogLevel::ERROR, std::forward<Args>(args)...);
+  void err(LogFormat format, Args&&... args) {
+    log(LogLevel::ERROR, format, std::forward<Args>(args)...);
   }
 
   template <typename... Args>
-  void warn(std::string_view fmt, Args&&... args) {
-    base(fmt, LogLevel::WARN, std::forward<Args>(args)...);
+  void warn(LogFormat format, Args&&... args) {
+    log(LogLevel::WARN, format, std::forward<Args>(args)...);
   }
 
   template <typename... Args>
-  void dbg(std::string_view fmt, Args&&... args) {
-#ifdef DEBUG
-    base(fmt, LogLevel::DBG, std::forward<Args>(args)...);
-#else
-    do {
-    } while (0);
+  void inf(LogFormat format, Args&&... args) {
+    log(LogLevel::INFO, format, std::forward<Args>(args)...);
+  }
+
+  template <typename... Args>
+  void dbg(LogFormat format, Args&&... args) {
+#if defined(DEBUG) || defined(_DEBUG)
+    log(LogLevel::DBG, format, std::forward<Args>(args)...);
 #endif
-  }
-  template <typename... Args>
-  void inf(std::string_view fmt, Args&&... args) {
-    base(fmt, LogLevel::INFO, std::forward<Args>(args)...);
   }
 
  private:
+  std::string m_name;
+  std::mutex m_mutex;
+
   template <typename... Args>
-  inline void base(std::string_view fmt, LogLevel lvl, Args&&... args) {
-    auto now = std::chrono::system_clock::now();
-    auto local_now = std::chrono::current_zone()->to_local(now);
-    std::string formatted_time = std::format("{:%Y-%m-%d %H:%M:%S}", local_now);
+  void log(LogLevel lvl, const LogFormat& format, Args&&... args) {
+    auto const time =
+        std::chrono::current_zone()->to_local(std::chrono::system_clock::now());
 
-    std::string msg = std::vformat(fmt, std::make_format_args(args...));
+    std::string user_msg =
+        std::vformat(format.fmt, std::make_format_args(args...));
+    std::string_view full_file_path = format.loc.file_name();
+    size_t sep_pos = full_file_path.rfind("/");
+    std::string_view short_file_name;
 
-    // ANSI color codes
-    const char* color = "";
-    switch (lvl) {
-      case WARN:
-        color = "\033[35m";
-        break;
-      case DBG:
-        color = "\033[36m";  // Cyan
-        break;
-      case ERROR:
-        color = "\033[31m";  // Red
-        break;
-      case INFO:
-        color = "\033[32m";  // Green
-        break;
-      default:
-        color = "\033[0m";  // Reset
-        break;
+    if (sep_pos != std::string::npos) {
+      short_file_name = full_file_path.substr(sep_pos + 1);
+    } else {
+      short_file_name = full_file_path;
     }
-    const char* reset = "\033[0m";
 
-    std::string const formatted =
-        std::format("{} - {}:{} - {} - {}\n", formatted_time, m_name, __LINE__,
-                    color + lvl_to_string(lvl) + reset, msg);
+    std::string final_output =
+        std::format("{:%Y-%m-%d %H:%M:%S} {}{}{} {}:{}: {}\n", time,
+                    lvl_to_color_code(lvl), lvl_to_string(lvl), "\033[0m",
+                    short_file_name, format.loc.line(), user_msg);
 
+    {
+      std::lock_guard lock(m_mutex);
+      if (lvl == LogLevel::ERROR) {
+        std::cerr << final_output;
+      } else {
+        std::cout << final_output;
+      }
+    }
+  }
+
+  constexpr std::string_view lvl_to_color_code(LogLevel lvl) {
     switch (lvl) {
-      case DBG:
-      case ERROR:
-      case WARN:
-        std::cerr << formatted;
+      case LogLevel::DBG:
+        return "\033[36m";
         break;
-      case INFO:
-        std::cout << formatted;
+      case LogLevel::INFO:
+        return "\033[32m";
+        break;
+      case LogLevel::WARN:
+        return "\033[33m";
+        break;
+      case LogLevel::ERROR:
+        return "\033[31m";
         break;
       default:
+        return "\033[32m";
         break;
     }
   }
-  std::string lvl_to_string(LogLevel lvl) {
+
+  constexpr std::string_view lvl_to_string(LogLevel lvl) {
     switch (lvl) {
-      case DBG:
+      case LogLevel::DBG:
         return "DEBUG";
-      case INFO:
-        return "INFO";
-      case ERROR:
+      case LogLevel::INFO:
+        return "INFO ";
+      case LogLevel::WARN:
+        return "WARN ";
+      case LogLevel::ERROR:
         return "ERROR";
-      case WARN:
-        return "WARNING";
       default:
         return "UNKNOWN";
+        break;
     }
   }
-  std::string m_name;
 };
