b133d21b6d661dfc837f7dcfe219957dc662f817
Mosazghi <fest121@hotmail.com>
2026-05-06 21:47:37 +0200

    Add scrollbar

--- src/browser/Browser.cpp
+++ src/browser/Browser.cpp
@@ -1,13 +1,22 @@
 #include "Browser.hpp"
 #include <SFML/Graphics.hpp>
-#include <cstdint>
+#include "SFML/Graphics/RectangleShape.hpp"
 #include "SFML/Graphics/View.hpp"
 #include "const.hpp"
 #include "url/Url.hpp"
 namespace browser {
 
-Browser::Browser(sf::RenderWindow& window)
-    : m_window{window}, m_running{true} {}
+Browser::Browser(sf::RenderWindow& window) : m_window{window}, m_running{true} {
+  if (!m_font.loadFromFile(
+          "/usr/share/fonts/google-noto-sans-mono-cjk-vf-fonts/"
+          "NotoSansMonoCJK-VF.ttc")) {
+    logger.err("Error loading font\n");
+    m_running = false;
+    return;
+  }
+
+  m_scroll_bar.setFillColor(sf::Color::White);
+}
 
 void Browser::load(url::URL& url) {
   auto resp = url.request();
@@ -16,14 +25,6 @@ void Browser::load(url::URL& url) {
 }
 
 void Browser::spin() {
-  sf::Font font;
-  if (!font.loadFromFile("/usr/share/fonts/google-noto-sans-mono-cjk-vf-fonts/"
-                         "NotoSansMonoCJK-VF.ttc")) {
-    logger.err("Error loading font\n");
-    m_running = false;
-    return;
-  }
-
   while (m_running && m_window.isOpen()) {
     sf::Event event;
     while (m_window.pollEvent(event)) {
@@ -43,19 +44,22 @@ void Browser::spin() {
       }
 
       if (event.type == sf::Event::MouseWheelScrolled ||
-          event.type == sf::Event::KeyPressed) {
+          (event.type == sf::Event::MouseButtonPressed &&
+           event.mouseButton.button == sf::Mouse::Left)) {
         scrolldown(event);
       }
     }
 
-    m_window.clear(sf::Color(30, 30, 50));
-    draw(font);
+    m_window.clear();
+    draw();
 
     m_window.display();
   }
 }
 
-void Browser::draw(sf::Font& font) {
+void Browser::draw() {
+  constexpr auto scroll_bar_width{20};
+  // Text
   for (const auto& [x, y, c] : m_display_list) {
     if (y > static_cast<int>(m_scroll + m_window.getSize().y)) {
       continue;
@@ -65,11 +69,27 @@ void Browser::draw(sf::Font& font) {
     }
 
     sf::String glyph(c);
-    sf::Text ch(glyph, font, 12);
+    sf::Text ch(glyph, m_font, 12);
     ch.setFillColor(sf::Color::White);
-    ch.setPosition(x, y - m_scroll);
+    ch.setPosition(x - scroll_bar_width, y - m_scroll);
     m_window.draw(ch);
   }
+  // Scrollbar container
+  sf::RectangleShape scroll_bar_container(
+      sf::Vector2f(scroll_bar_width, m_window.getSize().y));
+  scroll_bar_container.setFillColor(sf::Color(128, 128, 128));
+  scroll_bar_container.setPosition(m_window.getSize().x - scroll_bar_width, 0);
+  m_window.draw(scroll_bar_container);
+
+  // Scrollbar
+  float last_elem_y = std::get<1>(m_display_list.back());
+  float window_y = m_window.getSize().y;
+  const int scroll_bar_height = window_y * (window_y / last_elem_y);
+  const int scroll_bar_y = m_scroll * (window_y / last_elem_y);
+  m_scroll_bar.setSize(sf::Vector2f(scroll_bar_width, scroll_bar_height));
+  m_scroll_bar.setPosition(m_window.getSize().x - scroll_bar_width,
+                           scroll_bar_y);
+  m_window.draw(m_scroll_bar);
 }
 
 void Browser::relayoutForCurrentWindowWidth() {
@@ -86,6 +106,20 @@ void Browser::scrolldown(const sf::Event& event) {
                      event.mouseWheelScroll.delta < 0) ||
                     (event.type == sf::Event::KeyPressed &&
                      event.key.code == sf::Keyboard::Down);
+  // get x and y of the mouse position relative to the window
+  if (event.type == sf::Event::MouseButtonPressed &&
+      event.mouseButton.button == sf::Mouse::Left) {
+    if (m_scroll_bar.getGlobalBounds().contains(
+            static_cast<float>(event.mouseButton.x),
+            static_cast<float>(event.mouseButton.y))) {
+      logger.dbg("Clicked on scroll bar");
+      m_scroll_bar.setFillColor(sf::Color::Red);
+    } else {
+      logger.dbg("Clicked outside scroll bar");
+      m_scroll_bar.setFillColor(sf::Color::White);
+    }
+  }
+
   if (scrollUp) {
     m_scroll = std::max(0, m_scroll - SCROLL_STEP);
   } else if (scrollDown) {
