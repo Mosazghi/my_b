1910b26de0fb9cfefb60e7067baebe1ebc8b8d62
Mosazghi <fest121@hotmail.com>
2026-07-18 15:38:15 +0200

    Add cleanup function

--- src/http/HttpClient.cpp
+++ src/http/HttpClient.cpp
@@ -83,8 +83,8 @@ HttpResult HttpClient::get(std::string_view url) {
     }
   }
 
+  static int num_requests = 0;
   auto perform_request = [&](const HttpReqParams& params) -> HttpResult {
-    static int num_requests = 0;
     num_requests++;
     std::optional<http::HttpResponse> response{};
 
@@ -195,6 +195,9 @@ HttpResult HttpClient::get(std::string_view url) {
                         .max_age = max_age};
     }
   }
+
+  // cleanups
+  [&]() { num_requests = 0; }();
 
   return result;
 }
